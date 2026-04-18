677e1864302cf92e808d65ddd97fe7153c55321b
Vasavi Bandaru <vasavibandaru72@gmail.com>
2026-04-19 01:27:33 +0530

    chore: initialize inventory-service project structure
    chore: initialize inventory-service project structure

---
+++ inventory-service/src/main/java/com/orderflow/inventoryservice/InventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.orderflow.inventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryServiceApplication.class, args);
+	}
+
+}
